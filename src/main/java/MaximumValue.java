27ad76132a8a32eac33d248c44a9cbf6c3cdf9a5
pooja todkar <poojatodkar124@gmail.com>
2019-11-29 10:47:07 +0530

    generic code adde

--- src/main/java/MaximumValue.java
+++ src/main/java/MaximumValue.java
@@ -1,23 +1,31 @@
-public class MaximumValue {
-    public static String maxNumber(String num1,String num2,String num3) {
+public class MaximumValue<T extends Comparable> {
+    T value1;
+    T value2;
+    T value3;
 
-        if(num1.compareTo(num2) > 0) {
-            return num1;
-        }
-        else if( num3.compareTo(num2) > 0){
-            return num3;
-        }
-        else {
-            return num2;
-        }
-
+    public MaximumValue(T value1, T value2, T value3) {
+        this.value1 = value1;
+        this.value2 = value2;
+        this.value3 = value3;
     }
 
-    public static void main(String[] args) {
-        String num1 = "abc";
-        String num2 = "xyz";
-        String num3 = "mno";
-        maxNumber(num1,num2,num3);
+    public <T extends Comparable> T maxNumber() {
+
+        T max = (T) value2;
+        if (value1.compareTo(max) > 0) {
+            max = (T) value1;
+        } else if (value3.compareTo(max) > 0) {
+            max = (T) value3;
+        }
+
+        return max;
     }
+        private static <T > void printMaxValue (T maximumValue){
+            System.out.println(maximumValue);
+        }
+
+        public static void main (String[]args){
+        }
+
 
 }
--- src/test/java/MaxmimumValueTest.java
+++ src/test/java/MaxmimumValueTest.java
@@ -3,11 +3,42 @@ import org.junit.Test;
 
 public class MaxmimumValueTest {
 
+    @Test
+    public void givenThreeIntegerValue_WhenProper_ReturnTrue() {
+        Integer a = 1;
+        Integer b = 7;
+        Integer c = 8;
+        MaximumValue<Integer> integerFindMaxNum = new MaximumValue<>(a, b, c);
+        Comparable maxValue = integerFindMaxNum.maxNumber();
+        Assert.assertEquals(8,maxValue);
+    }
 
     @Test
-    public void maximumNumberFloat(){
-        MaximumValue maximumNumber = new MaximumValue();
-        String maxNumInteger = maximumNumber.maxNumber("abc","xyz","mno");
-        Assert.assertEquals("xyz",maxNumInteger);
+    public void givenThreeFloatValue_WhenProper_ReturnTrue() {
+        Float a = 1.5f;
+        Float b = 7.8f;
+        Float c = 8.5f;
+        MaximumValue<Float> floatFindMaxNum = new MaximumValue<Float>(a, b, c);
+        Comparable maxValue = floatFindMaxNum.maxNumber();
+        Assert.assertEquals(8.5f,maxValue);
+    }
+
+    @Test
+    public void givenThreeStringValue_WhenProper_ReturnTrue() {
+        String a = "POOJA";
+        String b = "xyz";
+        String c = "mno";
+        MaximumValue<String> stringFindMaxNum = new MaximumValue<String>(a, b, c);
+        Comparable maxValue = stringFindMaxNum.maxNumber();
+        Assert.assertEquals("Ram",maxValue);
+    }
+    @Test
+    public void givenThreeIntegerValue_WhenProper_ReturnPrintThatValue() {
+        Integer a = 10;
+        Integer b = 20;
+        Integer c = 30;
+        MaximumValue<Integer> integerFindMaxNum = new MaximumValue<Integer>(a, b, c);
+        Comparable maxValue = integerFindMaxNum.maxNumber();
+        Assert.assertEquals(30,maxValue);
     }
 }
